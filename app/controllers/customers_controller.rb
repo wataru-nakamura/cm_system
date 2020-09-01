ab22a1a7ef0cbc2a72a0a3f17cb4ae806fc77611
wataru-nakamura <wataru4711@gmail.com>
2020-09-01 18:03:49 +0900

    顧客登録機能

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,6 +3,17 @@ class CustomersController < ApplicationController
   end
 
   def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    binding.pry
+    if @customer.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -11,5 +22,12 @@ class CustomersController < ApplicationController
   def destory
   end
 
+  private
+
+  def customer_params
+   params.require(:customer).permit(:family_name, :first_name, :family_name_kana,
+   :first_name_kana, :birthday, :age, :postal_code, :address, :building_name,
+   :phone_number, :email)
+  end
 
 end
